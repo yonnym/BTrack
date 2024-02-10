689186bd136052c94476dd10cbd5248f9f4bb0af
YT <yonnym@gmail.com>
2024-02-10 18:54:38 +0000

    update CMakeLists.txt OnsetDetectionFunction.cpp and example

--- modules-and-plug-ins/python-module/example.py
+++ modules-and-plug-ins/python-module/example.py
@@ -1,5 +1,25 @@
 # need scikits audiolab for reading audio files
-from scikits.audiolab import wavread
+# from scikits.audiolab import wavread
+import pysndfile
+
+def wavread(filename):
+  last = None
+  first = 0
+  hdl = pysndfile.PySndfile(filename, 'r')
+  try:
+    fs = hdl.samplerate()
+    enc = hdl.encoding_str()
+    nf  = hdl.seek(first, 1)
+    if not nf == first:
+        raise IOError("Error while seeking at starting position")
+    if last is None:
+        nframes = hdl.frames() - first
+        data    = hdl.read_frames(nframes)
+    else:
+        data    = hdl.read_frames(last)
+  finally:
+    hdl.close()
+  return data, fs, enc
 
 # import numpy (needed to convert stereo audio to mono)
 import numpy as np
@@ -8,19 +28,20 @@ import numpy as np
 import btrack
 
 # set the path to an audio file on your machine
-audioFilePath = "/path/to/your/audioFile.wav"
+audioFilePath = "/data/audio.wav"
 
 # read the audio file
 audioData, fs, enc = wavread (audioFilePath)     # extract audio from file
 
 # convert to mono if need be
 if (audioData[0].size == 2):
-    print "converting to mono"
+    print( "converting to mono")
     audioData = np.average (audioData, axis = 1)
 
 # ==========================================    
 # Usage A: track beats from audio            
 beats = btrack.trackBeats (audioData)    
+print(beats)
 
 # ==========================================
 # Usage B: extract the onset detection function
@@ -29,3 +50,4 @@ onsetDF = btrack.calculateOnsetDF (audioData)
 # ==========================================
 # Usage C: track beats from the onset detection function (calculated in Usage B)
 ODFbeats = btrack.trackBeatsFromOnsetDF (onsetDF)
+print(ODFbeats)
